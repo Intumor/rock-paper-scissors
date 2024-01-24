ee11fb0b6e56c90c494745ba58e9213b3abf8b44
Alex Band <lingualex12345@gmail.com>
2024-01-24 13:52:06 -0600

    Finish the project

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,10 @@
+let roundsPlayed = 0;
+const score = {
+  wins: 0,
+  losses: 0,
+  ties: 0
+}
+
 const getComputerChoice = () => {
   const randomNumber = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
   let computerMove;
@@ -12,39 +19,78 @@ const getComputerChoice = () => {
   return computerMove;
 }
 
-const playRound = (playerMove, computerMove) => {
+const playRound = () => {
+  let playerMove = prompt('Enter your move');
+
+  playerMove = playerMove.toLowerCase();
+  const computerMove = getComputerChoice();
 
 //first if your move is rock
   if (playerMove === 'rock' && computerMove === 'rock') {
-    return `It's a tie! ${playerMove} can't beat ${computerMove}`;
+    score.ties++;
+    return `It's a tie!
+Your Move: ${playerMove}, Computer Move: ${computerMove}`;
   } else if (playerMove === 'rock' && computerMove === 'paper') {
-    return `You lose! ${computerMove} beats ${playerMove}`;
+    score.losses++;
+    return `You lose! ${computerMove} beats ${playerMove}
+Your Move: ${playerMove}, Computer Move: ${computerMove}`;
   } else if (playerMove === 'rock' && computerMove === 'scissors') {
-    return `You win! ${playerMove} beats ${computerMove}`;
+    score.wins++;
+    return `You win! ${playerMove} beats ${computerMove}
+Your Move: ${playerMove}, Computer Move: ${computerMove}`;
   }
 
   //if you choose paper
 
   if (playerMove === 'paper' && computerMove === 'rock') {
-    return `You win! ${playerMove} beats ${computerMove}`;
+    score.wins++;
+    return `You win! ${playerMove} beats ${computerMove}
+Your Move: ${playerMove}, Computer Move: ${computerMove}`;
   } else if (playerMove === 'paper' && computerMove === 'paper') {
-    return `It's a tie! ${playerMove} can't beat ${computerMove}`;
+    score.ties++;
+    return `It's a tie!
+Your Move: ${playerMove}, Computer Move: ${computerMove}`;
   } else if (playerMove === 'paper' && computerMove === 'scissors') {
-    return `You lose! ${computerMove} beats ${playerMove}`;
+    score.losses++;
+    return `You lose! ${computerMove} beats ${playerMove}
+Your Move: ${playerMove}, Computer Move: ${computerMove}`;
   }
 
   //if you choose scissors
 
   if (playerMove === 'scissors' && computerMove === 'rock') {
-    return `You lose! ${computerMove} beats ${playerMove}`;
+    score.losses++;
+    return `You lose! ${computerMove} beats ${playerMove}
+Your Move: ${playerMove}, Computer Move: ${computerMove}`;
   } else if (playerMove === 'scissors' && computerMove === 'paper') {
-    return `You win! ${playerMove} beats ${computerMove}`;
+    score.wins++;
+    return `You win! ${playerMove} beats ${computerMove}
+Your Move: ${playerMove}, Computer Move: ${computerMove}`;
   } else if (playerMove === 'scissors' && computerMove === 'scissors') {
-    return `It's a tie! ${playerMove} can't beat ${computerMove}`;
+    score.ties++;
+    return `It's a tie!
+Your Move: ${playerMove}, Computer Move: ${computerMove}`;
   }
 }
 
+const game = () => {
+  if (roundsPlayed < 5) {
+    roundsPlayed++
+    return playRound()
+  } else {
+    if (score.wins > score.losses) {
+      return `Game Over.
+You won the round! Final score: wins: ${score.wins}, losses: ${score.losses}, ties: ${score.ties}`;
+    } else if (score.wins === score.losses) {
+      return `Game Over.
+You tied the round! Final score: wins: ${score.wins}, losses: ${score.losses}, ties: ${score.ties}`;
+    } else if (score.wins < score.losses) {
+      return `Game Over.
+Computer won the round! Final score: wins: ${score.wins}, losses: ${score.losses}, ties: ${score.ties}`;
+    }
+  }
+}
 
-const playerMove = 'paper';
-const computerMove = getComputerChoice();
-console.log(playRound(playerMove, computerMove));
+for (let i = 0; i < 6; i++) {
+  console.log(game());
+}
